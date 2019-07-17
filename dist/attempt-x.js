c5ddfa138bf22dc68fb763c2612a14ae276a11c1
Graham Fairweather <xotic750@gmail.com>
2019-07-18 01:16:17 +0200

    :bookmark: v2.0.9
    Update dependencies

--- dist/attempt-x.js
+++ dist/attempt-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-17T18:13:24.801Z",
+  "date": "2019-07-17T23:16:13.107Z",
   "describe": "",
   "description": "Invokes function, returning an object of the results.",
   "file": "attempt-x.js",
   "hash": "c1f519133f7e8a2ee7ab",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
