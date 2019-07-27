9d5e6d5af3005a9ce86db6f4b44221cdbc1fbe3c
Graham Fairweather <xotic750@gmail.com>
2019-07-27 15:16:32 +0200

    :bookmark: v2.0.22
    Auto build

--- dist/attempt-x.js
+++ dist/attempt-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-27T00:15:14.326Z",
+  "date": "2019-07-27T13:16:27.436Z",
   "describe": "",
   "description": "Invokes function, returning an object of the results.",
   "file": "attempt-x.js",
   "hash": "c1f519133f7e8a2ee7ab",
   "license": "MIT",
-  "version": "2.0.21"
+  "version": "2.0.22"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
