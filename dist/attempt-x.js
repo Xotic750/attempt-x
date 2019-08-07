d7f7a4e732df7d46d41e31d4bc8e5d421d637206
Graham Fairweather <xotic750@gmail.com>
2019-08-07 02:49:36 +0200

    Auto build

--- dist/attempt-x.js
+++ dist/attempt-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-06T18:45:17.991Z",
+  "date": "2019-08-07T00:49:31.038Z",
   "describe": "",
   "description": "Invokes function, returning an object of the results.",
   "file": "attempt-x.js",
